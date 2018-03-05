7bb44dbd1c7866f6dcfa1577cd76394cc8eaa157
宮﨑琴音_高松P <ackanabuki02@gmail.com>
2018-03-05 11:26:54 +0900

    高松

--- Hiroshima/BaceProject/GameProject/Game/CItem.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "CBase.h"
-
-class CItem :public CBase {
-public:
-	CItem(CVector2D pos);
-	~CItem();
-	void Update();
-};
--- Hiroshima/BaceProject/GameProject/Game/CScore.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "CBase.h"
-
-class CScore :public CBase {
-public:
-	CScore();
-	~CScore();
-	void Draw();
-};
