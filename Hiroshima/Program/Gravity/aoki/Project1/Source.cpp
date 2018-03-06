71d5f1aed5b772b407c89cf4b50c00f154bf133a
青木悟志 <ackanabuki00@gmail.com>
2018-03-06 18:16:14 +0900

    j

--- Hiroshima/Program/Gravity/aoki/Project1/Source.cpp
+++ Hiroshima/Program/Gravity/aoki/Project1/Source.cpp
@@ -1,45 +1,35 @@
-#include"stdio.h"
+#include "stdio.h"
 
-const int  FPS = 60;
+const int FPS = 60;
 
-struct Vector3
-{
-
-	float x,y,z;
-
+struct Vector3 {
+	float x, y, z;
 };
 
 Vector3 Update(Vector3 pos, Vector3 speed, Vector3 gravity) {
-	pos.x += speed.x + gravity.x;
-	pos.y += speed.y + gravity.y;
-	pos.z += speed.z + gravity.z;
+	pos.x += speed.x;
+	pos.y += speed.y + gravity.y / 2;
+	pos.z += speed.z;
 	return pos;
 }
 
-void main(){
-	Vector3 pos;   //POi‰ŠúˆÊ’u‚ğ‘ã“ü‚µ‚Äj
+void main() {
+	Vector3 pos;
 	pos.x = 2.0f;
 	pos.y = 3.0f;
 	pos.z = 4.0f;
-	Vector3 speed; //–â7‚Ì“š‚¦
+	Vector3 speed;
 	speed.x = 1.6f;
 	speed.y = 4.9f;
-	speed.z = -2.5;
+	speed.z = -2.5f;
 	Vector3 gravity;
 	gravity.x = 0.0f;
 	gravity.y = -9.8f;
 	gravity.z = 0.0f;
-
-
-	//‚P•b
-	for (int i = 0; i < 5*FPS; ++i) {
+	//1•b
+	for (int i = 0; i < 5; ++i) {
 		pos = Update(pos, speed, gravity);
 	}
 	printf("%f %f %f", pos.x, pos.y, pos.z);
 	getchar();
 }
-
-void Update() {
-
-}
-
