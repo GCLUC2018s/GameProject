594b22c3b9b06468fce6d675e0b8ed53727b86a6
宮﨑琴音_高松P <ackanabuki02@gmail.com>
2018-03-07 16:37:46 +0900

    takamatu

---
+++ Hiroshima/Program/Gravity/Miyazaki/Task/Task/TaskManager.h
@@ -0,0 +1,18 @@
+#ifndef TASK_MANAGER_GUARD
+#define TASK_MANAGER_GUARD
+#include "Task.h"
+
+class TaskManager
+{
+public:
+	TaskManager() {
+	}
+	void Update() {
+
+	}
+	static TaskManager* m_instan;
+	void Add(Task* t) {
+	}
+};
+
+#endif // !TASK_MANAGER_GUARD
