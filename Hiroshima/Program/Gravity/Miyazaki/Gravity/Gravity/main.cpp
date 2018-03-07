3b0b6ddc5bf480c466ddfd01f2c1845d3a79830c
宮﨑琴音_高松P <ackanabuki02@gmail.com>
2018-03-07 09:46:51 +0900

    高松

--- Hiroshima/Program/Gravity/Miyazaki/Gravity/Gravity/main.cpp
+++ Hiroshima/Program/Gravity/Miyazaki/Gravity/Gravity/main.cpp
@@ -5,7 +5,7 @@ struct Vector3
 	float x, y, z;
 };
 
-Vector3 Update(Vector3 u_pos, Vector3 u_speed, Vector3 u_gravity,float deltatime) {
+void Update(Vector3& u_pos, Vector3& u_speed, const Vector3 u_gravity,float deltatime) {
 	//そくど
 	u_speed.x += u_gravity.x * deltatime;
 	u_speed.z += u_gravity.z * deltatime;
@@ -15,27 +15,25 @@ Vector3 Update(Vector3 u_pos, Vector3 u_speed, Vector3 u_gravity,float deltatime
 	u_pos.x += u_speed.x * deltatime;
 	u_pos.z += u_speed.z * deltatime;
 	u_pos.y += u_speed.y * deltatime;
-
-	return u_pos;
 }
 
 void main() {
 	Vector3 pos;	//P0（初期位置を代入して）
-	pos.x = 2.0, pos.y = 3.0, pos.z = -4.0;
-	Vector3 pos1;	//P1（初期位置を代入して）
-	pos1.x = 10.0, pos1.y = 5.0, pos1.z = -8.5;
+	pos.x = 2.0f, pos.y = 3.0f, pos.z = -4.0f;
 	Vector3 speed;
-	speed.x = pos1.x - pos.x;
-	speed.y = pos1.y - pos.y;
-	speed.z = pos1.z - pos.z;
+	speed.x = 1.6f;
+	speed.y = 4.9f;
+	speed.z = -2.5f;
+	//speed.x = pos1.x - pos.x;
+	//speed.y = pos1.y - pos.y;
+	//speed.z = pos1.z - pos.z;
 	Vector3 gravity;
-	gravity.x = 0.0, gravity.y = 9.8, gravity.z = 0.0;
-	Vector3 an_pos;
+	gravity.x = 0.0f, gravity.y = -9.8f, gravity.z = 0.0f;
 
 	for (int i = 0; i < 5 * FPS; i++) {
-		an_pos = Update(pos, speed, gravity, 1.0f / (float)FPS);
+		Update(pos, speed, gravity, 1.0f / (float)FPS);
 	}
-	printf("%f,%f,%f", an_pos.x, an_pos.y, an_pos.z);
+	printf("%f,%f,%f", pos.x, pos.y, pos.z);
 	getchar();
 }
 
