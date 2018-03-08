456a253ad1b3b93af800cf6d9b543a732f404ed7
Nagao-Ren-Fukuyama <17010511@anabuki-net.ne.jp>
2018-03-08 18:33:39 +0900

    日報の更新と、タスクの課題１を更新しました。

---
+++ Fukuyama/program/task/長尾/タスク課題2/GameProgramming/CPlayer.cpp
@@ -0,0 +1,31 @@
+#include "CPlayer.h"
+#include "random"
+#define KEY(a) GetKeyState(a)&0x8000
+float Erand = rand()%GLUT_WINDOW_WIDTH;
+void CPlayer::Init(){
+	mTexture.Load("Player.tga");
+	mTexture.DrawImage(mLeft, mRight, mBottom, mTop, 0, 32, 32, 0);
+	SetVer(-16, 16, -16, 16);
+}
+void CPlayer::Update(){
+	if (KEY('W')){
+		mTop += 3;
+		mBottom += 3;
+	}
+	if (KEY('A')){
+		mLeft -= 3;
+		mRight -= 3;
+	}
+	if (KEY('S')){
+		mBottom -= 3;
+		mTop -= 3;
+	}
+	if (KEY('D')){
+		mLeft += 3;
+		mRight += 3;
+	}
+}
+void CPlayer::Draw(){
+	mEnabled = true;
+	CRectangle::Render();
+}
