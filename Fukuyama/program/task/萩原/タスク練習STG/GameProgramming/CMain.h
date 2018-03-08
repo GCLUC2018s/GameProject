fe0ae341fefefe83af01f16f8e5375ae35f7fc99
Hagiwara-Yohei-Fukuyama <17020513@anabuki-net.ne.jp>
2018-03-08 16:20:26 +0900

    タスク練習用シューティングを作成しました。

---
+++ Fukuyama/program/task/萩原/タスク練習STG/GameProgramming/CMain.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "glut.h"
+#include "CRectangle.h"
+
+#define W_H		1200 //Windowïù
+#define W_V		900 //WindowçÇÇ≥
+
+class CMain {
+	int		mState;
+public:
+	CRectangle *Player;
+	CMain() : mState(0) {}
+
+	void MainLoop() {
+		switch (mState)
+		{
+		case 0:
+			Init();
+			mState = 1;
+			break;
+		default:
+			Update();
+			break;
+		}
+	}
+	void Init();
+	void Update();
+
+};
+
