e56adacc4e1dfc8c2389f6586460d257429129c8
Kiyohara-kazuma-tokusima <36870772+Kiyohara-kazuma-tokusima@users.noreply.github.com>
2018-03-13 16:40:32 +0900

    エネミーの出現まで作成しました。

---
+++ Tokushima/program/game/CodeCoat_enemyの出現/CodeCoat_本データ/Task/CTaskLinker.h
@@ -0,0 +1,40 @@
+/**
+* @file		CTaskLinker.h
+* @brief	タスクシステムのリストを形成するためのポインタクラス 
+* @author	ryoji anzai,yuki yamaji
+*/
+
+#ifndef TASK_LINKER_GUARD
+#define TASK_LINKER_GUARD
+
+class CTask;
+
+class CTaskLinker
+{
+	friend class CTaskManager;
+	friend class CPlayerManager;
+	friend class CItemManager;
+
+private:
+	int			m_priority;			// 優先度 
+	CTaskLinker	*mp_prev;			// 前のポインタ
+	CTaskLinker	*mp_next;			// 次のポインタ
+	CTask		*mp_task;			// 自身 
+
+public:
+	// コンストラクタ 
+	CTaskLinker();
+	// コンストラクタ 
+	CTaskLinker(int priority, CTask *task);
+
+	// デストラクタ 
+	~CTaskLinker();
+
+	// 優先度設定 
+	void	SetPriority( int prio );
+	// 優先度取得 
+	int		GetPriority() const;
+
+};
+
+#endif
---
+++ Tokushima/program/game/CodeCoat_enemyの出現/CodeCoat_本データ/ui.h
@@ -0,0 +1,11 @@
+#ifndef UI_H
+#define UI_H
+
+#include "Task\CTask.h"
+
+class Ui :public CTask{
+private:
+	int timer_img[];
+};
+
+#endif UI_H
