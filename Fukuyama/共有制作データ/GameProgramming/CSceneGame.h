79e2328c01657efaabb33d3af508ce0e9da2eb90
Hakozaki-hayate-Fukuyama <17010514@anabuki-net.ne.jp>
2018-03-13 10:54:40 +0900

    復旧

---
+++ Fukuyama/共有制作データ/GameProgramming/CSceneGame.h
@@ -0,0 +1,18 @@
+#ifndef CSCENEGAME_H
+#define CSCENEGAME_H
+<<<<<<< HEAD
+#include"task\CTask.h"
+#include"draw\CTexture.h"
+class CSceneGame :public CTask{
+public:
+	CTexture mTexture;
+	CTexture mTexture2;
+	int m_PawsCount;
+	int GameScene;
+	void Init();
+	void Update();
+};
+=======
+
+>>>>>>> 7100b3b70f3e929a34669491262110dbc26a1e45
+#endif
