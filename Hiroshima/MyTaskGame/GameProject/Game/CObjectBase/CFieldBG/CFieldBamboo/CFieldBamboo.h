5a4aac9a72fe5c936ada3cd24e25ba6893b6aab9
六車友梨 <ackanabuki03@gmail.com>
2018-03-13 12:47:24 +0900

    追加

---
+++ Hiroshima/MyTaskGame/GameProject/Game/CObjectBase/CFieldBG/CFieldBamboo/CFieldBamboo.h
@@ -0,0 +1,18 @@
+#ifndef FIELDBAMBOO_GUARD
+#define FIELDBAMBOO_GUARD
+
+#include"../GameProject/Game/CObjectBase/CObjectBase.h"
+
+/*
+
+êªçÏé“Å@òZé‘
+
+*/
+class CFieldBamboo :public CObjectBase {
+public:
+	CFieldBamboo();
+	~CFieldBamboo();
+	void Update();
+};
+
+#endif // !FIELDBAMBOO_GUARD
--- Hiroshima/MyTaskGame/GameProject/main.cpp
+++ Hiroshima/MyTaskGame/GameProject/main.cpp
@@ -74,8 +74,8 @@ void Init(void)
 	ADD_RESOURCE("Title_StartText", CAnimImage::LoadImage("item_huda_1.png"));
 	ADD_RESOURCE("Sky", CAnimImage::LoadImage("bg_sky_01.png"));
 	ADD_RESOURCE("Cloud", CAnimImage::LoadImage("bg_cloud.png"));
-//	ADD_RESOURCE("Bamboo", CAnimImage::LoadImage("bg_bamboo.png"));
-//	ADD_RESOURCE("Ground", CAnimImage::LoadImage("bg_ground"));
+	ADD_RESOURCE("Bamboo", CAnimImage::LoadImage("bg_bamboo.png"));
+	ADD_RESOURCE("Ground", CAnimImage::LoadImage("bg_ground.png"));
 
 	SOUND("BGM_Title")->Load("Sound/BGM/Enishi.wav");
 	SOUND("BGM_Main")->Load("Sound/BGM/n82.wav");
