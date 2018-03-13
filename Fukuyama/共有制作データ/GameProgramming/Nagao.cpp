9a5c00d73a2062a5ce84da1febd452e1e5c0e3e5
Nagao-Ren-Fukuyama <17010511@anabuki-net.ne.jp>
2018-03-13 19:14:15 +0900

    コミット

--- Fukuyama/共有制作データ/GameProgramming/Nagao.cpp
+++ Fukuyama/共有制作データ/GameProgramming/Nagao.cpp
@@ -1,1 +1,6 @@
 #include "Nagao.h"
+
+Nagao::Nagao()
+{
+	Manhole = new C_Manhole();
+}
--- Fukuyama/共有制作データ/GameProgramming/Nagao.h
+++ Fukuyama/共有制作データ/GameProgramming/Nagao.h
@@ -1,9 +1,11 @@
 #ifndef NAGAO_H
 #define NAGAO_H
-class Nagao{
+#include "object\CManhole.h"
+class Nagao : public C_Manhole{
 public:
-
+	C_Manhole* Manhole;
+	//テスト用コンストラクタを作成
+	Nagao();
 private:
-
 };
 #endif
