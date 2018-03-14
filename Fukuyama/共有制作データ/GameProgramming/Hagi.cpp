8a8052e08fd3904adae10f0993bda6cbe11823b8
Hagiwara-Yohei-Fukuyama <17020513@anabuki-net.ne.jp>
2018-03-14 17:39:50 +0900

    スクロール処理を追加しました。

--- Fukuyama/共有制作データ/GameProgramming/Hagi.cpp
+++ Fukuyama/共有制作データ/GameProgramming/Hagi.cpp
@@ -2,8 +2,11 @@
 
 void Hagi::Update(){
 	C_Player::Update();
+	Test->Update();
+	//Test->m_Scroll = Player->m_Scroll;
 }
 
 void Hagi::Draw(){
 	C_Player::Draw();
+	Test->Draw();
 }
--- Fukuyama/共有制作データ/GameProgramming/draw/CRectangle.cpp
+++ Fukuyama/共有制作データ/GameProgramming/draw/CRectangle.cpp
@@ -40,18 +40,3 @@ float C_Rectangle::m_PotisionX(float m_H){
 	m_Right += mh;
 	return mh;
 }
-
-//スクロールの基準点が一定値を超えていたらスクロールする。
-void C_Rectangle::Scroll(C_Rectangle*p){
-
-	//idがUIでないならスクロールします
-	if (m_Scroll >= SCROLL_POINT){
-		//スクロールの基準点が一定値より大きくなるとスクロールする
-		if (CKey::Push(VK_RIGHT)){
-			p->m_Right -= SCROLL_SPEED;
-			p->m_Left -= SCROLL_SPEED;
-		}
-	}
-	return;
-}
-
