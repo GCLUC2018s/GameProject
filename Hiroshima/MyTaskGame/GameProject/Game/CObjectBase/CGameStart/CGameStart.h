9a1faf4acb0c8faba8cde2a64b34addbbe20732e
宮﨑琴音_高松P <ackanabuki02@gmail.com>
2018-03-14 13:44:33 +0900

    競合起きる前に

---
+++ Hiroshima/MyTaskGame/GameProject/Game/CObjectBase/CGameStart/CGameStart.h
@@ -0,0 +1,28 @@
+#ifndef GAME_START_GUARD
+#define GAME_START_GUARD
+
+#define GAME_START_X 711
+#define GAME_START_Y 76
+
+#define GAME_START_TIME 120
+
+#include "../CObjectBase.h"
+
+/*
+
+制作者　宮﨑
+
+*/
+class CGameStart : public CObjectBase
+{
+public:
+	CGameStart();
+	~CGameStart();
+	void Update();
+private:
+	float	m_siz;	//サイズ用
+	float	m_up_siz;	//増やす量
+	bool	m_end_flag;	//終わりますよフラグ
+	int		m_cnt;	//フェードアウト開始。
+};
+#endif // !GAME_START_GUARD
--- Hiroshima/MyTaskGame/GameProject/Game/CObjectBase/CTitleBG/CTitleBG.cpp
+++ Hiroshima/MyTaskGame/GameProject/Game/CObjectBase/CTitleBG/CTitleBG.cpp
@@ -6,6 +6,7 @@ CTitleBG::CTitleBG() :CObjectBase(0, eU_Back, eD_Null)
 	m_img = *dynamic_cast<CAnimImage*>(GET_RESOURCE("Title"));
 	m_img.SetSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 	m_end_flag = false;
+	m_color.a = 0.0;
 }
 
 CTitleBG::~CTitleBG()
@@ -18,10 +19,13 @@ CTitleBG::~CTitleBG()
 void CTitleBG::Update()
 {
 	m_img.SetColor(m_color.r, m_color.g, m_color.b, m_color.a);
-	if (m_end_flag == false && PUSH_ENTER) {
-		SOUND("SE_Title_Enter")->Play(false);
-		m_end_flag = true;
-		m_color.a = 2.0;
+	if (m_end_flag == false) {
+		m_color.a += 0.05;
+		if (PUSH_ENTER) {
+			SOUND("SE_Title_Enter")->Play(false);
+			m_end_flag = true;
+			m_color.a = 2.0;
+		}
 	}
 	if (m_end_flag) {
 		m_color.a -= 0.01;
