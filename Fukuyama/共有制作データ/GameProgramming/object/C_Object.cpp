9efa311073ae673d4117ae186c0775703eb28cf2
Nagao-Ren-Fukuyama <17010511@anabuki-net.ne.jp>
2018-03-15 10:50:09 +0900

    マンホールのTGAデータを削除しているので、コメント化しといてね

--- Fukuyama/共有制作データ/GameProgramming/object/C_Object.cpp
+++ Fukuyama/共有制作データ/GameProgramming/object/C_Object.cpp
@@ -2,7 +2,7 @@
 
 float C_Object::m_Scroll = SCROLL_POINT;
 
-void C_Object::Rect(C_Rectangle *m_image,C_Vector3* pos){
+void C_Object::Rect(C_Rectangle *m_image,const C_Vector3* pos){
 	m_image->m_wide = (m_image->m_Right - m_image->m_Left) / 2;  //ポジションからC_Rectangleの左右までの距離
 	m_image->m_high = (m_image->m_Top - m_image->m_Bottom);  //高さ
 	m_image->m_Right = pos->x + m_image->m_wide;
