b73e477649a9890b276b19f1482b9cedf407a888
Hagiwara-Yohei-Fukuyama <17020513@anabuki-net.ne.jp>
2018-03-15 18:56:48 +0900

    バグを修正しました

--- Fukuyama/共有制作データ/GameProgramming/object/C_Object.h
+++ Fukuyama/共有制作データ/GameProgramming/object/C_Object.h
@@ -13,6 +13,7 @@ public:
 		, init(false)
 		, m_Position(posx, posy, posz)
 		, m_Speed(0.0f, 0.0f, 0.0f)
+		, m_Gravity(0.0f,-9.8f,0.0f)
 	{}
 	bool init;  //Initを通ったかどうか
 	void Init();
@@ -21,6 +22,7 @@ public:
 protected:
 	C_Rectangle m_image;  //メンバー変数にする
 	C_Vector3 m_Speed;  //速度を管理
+	C_Vector3 m_Gravity;  //重力
 	void Rect(C_Rectangle* m_image,const C_Vector3* pos);  //ポジションの場所にC_Rectangle表示
 	void Scroll(C_Vector3 *m_pos,float m_Scroll);     //スクロール処理をします
 };
