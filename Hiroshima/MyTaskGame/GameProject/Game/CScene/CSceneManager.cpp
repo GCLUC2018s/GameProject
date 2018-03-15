81afabdb2e8ad2085d64f94b753a2c36784792be
河野_十儀_高松 <ackanabuki01@gmail.com>
2018-03-15 16:00:52 +0900

    お札

--- Hiroshima/MyTaskGame/GameProject/Game/CScene/CSceneManager.cpp
+++ Hiroshima/MyTaskGame/GameProject/Game/CScene/CSceneManager.cpp
@@ -68,7 +68,6 @@ void CSceneManager::MainScene(){
 	new CFieldBamboo();
 	new CFieldGround();
 
-	new COhuda(&(CVector3D(500,500,0)));
 	new CKoban(&(CVector3D(500,500, 0)));
 	new COage(&(CVector3D(500, 450, 0)));
 
