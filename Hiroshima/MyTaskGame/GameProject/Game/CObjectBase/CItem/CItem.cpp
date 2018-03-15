8cbfbbd8d156b366ecfa916db53d53c2d8105496
六車友梨 <ackanabuki03@gmail.com>
2018-03-15 13:04:20 +0900

    takamatu

--- Hiroshima/MyTaskGame/GameProject/Game/CObjectBase/CItem/CItem.cpp
+++ Hiroshima/MyTaskGame/GameProject/Game/CObjectBase/CItem/CItem.cpp
@@ -2,6 +2,7 @@
 
 CItem::CItem(CVector3D * pos):CObjectBase(0,eU_Item,eD_Item){
 	m_img = *dynamic_cast<CAnimImage*>(GET_RESOURCE("Ohuda"));
+	m_img.SetRect(0, 0, 128, 128);
 	m_img.SetSize(64,64);
 	m_pos3D = *pos;
 	
