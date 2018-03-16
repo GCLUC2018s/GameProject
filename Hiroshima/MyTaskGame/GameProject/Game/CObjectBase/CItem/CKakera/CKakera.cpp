6d6c20dde3d8e9e89bd0448d0df30291e27b0360
六車友梨 <ackanabuki03@gmail.com>
2018-03-16 18:02:26 +0900

    いろいろ

--- Hiroshima/MyTaskGame/GameProject/Game/CObjectBase/CItem/CKakera/CKakera.cpp
+++ Hiroshima/MyTaskGame/GameProject/Game/CObjectBase/CItem/CKakera/CKakera.cpp
@@ -3,6 +3,7 @@
 CKakera::CKakera(CVector3D * pos) :CObjectBase(0, eU_Item, eD_Object) {
 	m_img = *dynamic_cast<CAnimImage*>(GET_RESOURCE("Kakera"));
 	m_img.SetSize(32, 32);
+	m_rect = CRect(0, 0, 32, 32);
 	m_pos3D = *pos;
 
 }
@@ -12,4 +13,20 @@ CKakera::~CKakera() {
 }
 
 void CKakera::Update() {
+}
+
+void CKakera::Draw() {
+
+	m_img.SetColor(m_color.x, m_color.y, m_color.z, m_color.w);
+	m_img.SetFlipH(m_flipH);
+
+	m_img.SetPos(m_pos3D.x - m_pos3D.z / 7 - m_scroll.x, 450 + m_pos3D.y + m_pos3D.z / 2 - m_scroll.y);
+	m_img.Draw();
+	Utility::DrawQuad(CVector2D(m_pos3D.x - m_pos3D.z / 7 - m_scroll.x + m_rect.m_left, 450 + m_pos3D.y + m_pos3D.z / 2 - m_scroll.y + m_rect.m_top), CVector2D(m_rect.m_right - m_rect.m_left, m_rect.m_bottom - m_rect.m_top), CVector4D(1, 0, 0, 0.3));
+	Utility::DrawQuad(CVector2D(m_pos3D.x - m_pos3D.z / 7 - m_scroll.x + m_rect_F.m_left, 450 + m_pos3D.y + m_pos3D.z / 2 - m_scroll.y + m_rect_F.m_top), CVector2D(m_rect_F.m_right - m_rect_F.m_left, m_rect_F.m_bottom - m_rect_F.m_top), CVector4D(0, 0, 1, 0.2));
+
+}
+
+void CKakera::Hit(CTask * t)
+{
 }
