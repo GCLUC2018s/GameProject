609109801decbae81ba30d63afaf0a555c69bcf3
六車友梨 <ackanabuki03@gmail.com>
2018-03-16 12:40:06 +0900

    item

--- Hiroshima/MyTaskGame/GameProject/Game/CObjectBase/CItem/CKakera/CKakera.h
+++ Hiroshima/MyTaskGame/GameProject/Game/CObjectBase/CItem/CKakera/CKakera.h
@@ -4,11 +4,11 @@
 #include "../GameProject/Game/CObjectBase/CObjectBase.h"
 
 class CKakera :public CObjectBase {
+
 public:
 	CKakera(CVector3D * pos);
 	~CKakera();
 	void Update();
-	void Draw();
 };
 
 #endif // !KAKERA_GUARD
