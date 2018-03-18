672f880f3f2330051925bab4e17411a026c5753e
Hagiwara-Yohei-Fukuyama <17020513@anabuki-net.ne.jp>
2018-03-19 07:41:24 +0900

    設置型カラーボールを作成しました。

--- Fukuyama/共有制作データ/GameProgramming/object/CColorBall.cpp
+++ Fukuyama/共有制作データ/GameProgramming/object/CColorBall.cpp
@@ -4,49 +4,74 @@ void C_ColorBall::Init(){
 	i_Throwpos = C_Player::m_Playerpoint->m_Position;
 	//カラーボールの影とボールを同期
 	m_Position = i_Throwpos;
-	//カラーボールの初期位置の調整
-	//右向き
-	if (C_Player::m_Playerpoint->m_Turn == E_RIGHT){
-		m_Position.x += 40;
-		m_Position.z += 80;
-		//カラーボールの初速
-		m_Speed = C_Vector3(500, 4, 0);
+
+	//投げたカラーボールの場合
+	if (state == E_NAGE){
+		//カラーボールの初期位置の調整
+		//右向き
+		if (C_Player::m_Playerpoint->m_Turn == E_RIGHT){
+			m_Position.x += 40;
+			m_Position.z += 80;
+			//カラーボールの初速
+			m_Speed = C_Vector3(500, 4, 0);
+		}
+		//左向き
+		else if (C_Player::m_Playerpoint->m_Turn == E_LEFT){
+			m_Position.x -= 40;
+			m_Position.z += 80;
+			//カラーボールの初速
+			m_Speed = C_Vector3(-500, 4, 0);
+		}
 	}
-	//左向き
-	else if (C_Player::m_Playerpoint->m_Turn == E_LEFT){
-		m_Position.x -= 40;
-		m_Position.z += 80;
-		//カラーボールの初速
-		m_Speed = C_Vector3(-500, 4, 0);
+	else{
+		//カラーボールの初期位置の調整
+		//右向き
+		if (C_Player::m_Playerpoint->m_Turn == E_RIGHT){
+			m_Position.x += 35;
+		}
+		//左向き
+		else if (C_Player::m_Playerpoint->m_Turn == E_LEFT){
+			m_Position.x -= 35;
+		}
 	}
+
 }
 
 void C_ColorBall::Update()
 {
+	//Initを1度だけ呼びます（コンストラクタではm_PlayerpointがエラーになるためInitで処理します）
 	if (init == false){
 		Init();
 		init = true;
 	}
 
-	m_Speed.x += m_Gravity.x*m_Deltatime;
-	m_Speed.y += m_Gravity.y*m_Deltatime;
-	m_Speed.z += m_Gravity.z*m_Deltatime;
+	//投げたカラーボールの場合
+	if (state == E_NAGE){
+		//重力加速度を速度へ反映
+		m_Speed.x += m_Gravity.x*m_Deltatime;
+		m_Speed.y += m_Gravity.y*m_Deltatime;
+		m_Speed.z += m_Gravity.z*m_Deltatime;
 
-	m_Position.x += m_Speed.x*m_Deltatime;
-	m_Position.y += m_Speed.y*m_Deltatime;
-	m_Position.z += m_Speed.z*m_Deltatime;
+		//速度をもとにポジションを更新
+		m_Position.x += m_Speed.x*m_Deltatime;
+		m_Position.y += m_Speed.y*m_Deltatime;
+		m_Position.z += m_Speed.z*m_Deltatime;
 
-	m_Position.z += m_Speed.y;
+		m_Position.z += m_Speed.y;
 
-	if (m_Position.z <= i_Throwpos.z){
-		SetKill();
+		//カラーボールが地面に落ちると消します
+		if (m_Position.z <= i_Throwpos.z){
+			SetKill();
+		}
 	}
 
-	Rect(&m_image, &m_Position);
-	Scroll(&m_Position, m_Scroll);
+	Rect(&m_image, &m_Position);             //画像の位置をm_Positionと同期します
+	Scroll(&m_Position, m_Scroll);           //スクロール処理をします
 }
 
 void C_ColorBall::Draw(){
-	if (init==true)
+
+	//カラーボールの描画
 	i_ColorBall.DrawImage(m_image.m_Left, m_image.m_Right, m_image.m_Bottom, m_image.m_Top, 0, 90, 140, 5);
+
 }
--- Fukuyama/共有制作データ/GameProgramming/object/CColorBall.h
+++ Fukuyama/共有制作データ/GameProgramming/object/CColorBall.h
@@ -17,6 +17,10 @@ public:
 		, m_Deltatime(2.0f/FPS)
 		, init(false)
 	{
+		//カラーボールのステータスを設定します
+		if (CKey::Push('C'))state = E_NAGE;
+		if (CKey::Push('X'))state = E_SET;
+
 		//カラーボールのサイズ
 		m_image.SetVertex(0, 50, 0, 50);
 		i_ColorBall.Load("../image/chara/chara_motion_2_16bit.tga");
@@ -25,6 +29,7 @@ public:
 	CTexture i_ColorBall;
 	float m_Deltatime;     //1秒間の処理回数
 	C_Vector3 i_Throwpos;         //投げた位置
+	int state;               //投げた球か設置の球か
 	bool init;
 	void Init();
 	void Update();
