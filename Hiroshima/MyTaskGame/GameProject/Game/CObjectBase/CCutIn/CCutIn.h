3af18691b4f8cdc037798f81423ca9ac47a7c9bb
宮﨑琴音_高松P <ackanabuki02@gmail.com>
2018-03-19 16:12:59 +0900

    カットイン追加

---
+++ Hiroshima/MyTaskGame/GameProject/Game/CObjectBase/CCutIn/CCutIn.h
@@ -0,0 +1,21 @@
+#ifndef CUTIN_GUARD
+#define CUTIN_GUARD
+
+#include "../CObjectBase.h"
+
+/*
+
+制作者　宮﨑
+
+*/
+class CCutIn : public CObjectBase
+{
+public:
+	//カットイン
+	CCutIn();
+	~CCutIn();
+	void Update();
+
+};
+
+#endif // !CUTIN_GUARD
