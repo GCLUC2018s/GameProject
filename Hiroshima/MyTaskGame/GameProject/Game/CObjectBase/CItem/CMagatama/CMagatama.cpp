536f91d997f4bc26d6de18d5771eef8bbbe92b7f
宮﨑琴音_高松P <ackanabuki02@gmail.com>
2018-03-19 11:10:52 +0900

    Sauce追加

--- Hiroshima/MyTaskGame/GameProject/Game/CObjectBase/CItem/CMagatama/CMagatama.cpp
+++ Hiroshima/MyTaskGame/GameProject/Game/CObjectBase/CItem/CMagatama/CMagatama.cpp
@@ -3,7 +3,8 @@
 CMagatama::CMagatama() :CObjectBase(eID_Magatama, eU_Item, eD_Object) {
 	m_img = *dynamic_cast<CAnimImage*>(GET_RESOURCE("UI"));
 	m_img.SetRect(0, 0, 128, 128);
-	m_img.SetSize(128, 128);
+	m_img.SetSize(100, 100);
+	m_pos3D = CVector3D(20, 20, 0);
 	m_cnt = 0;
 }
 
