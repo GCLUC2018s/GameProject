9a192fb60ee4c12c4ad1dc3a15ea3225c4b2cf30
宮﨑琴音_高松P <ackanabuki02@gmail.com>
2018-03-20 10:29:58 +0900

    Score追加

--- Hiroshima/MyTaskGame/GameProject/Game/CObjectBase/CObjectBase.h
+++ Hiroshima/MyTaskGame/GameProject/Game/CObjectBase/CObjectBase.h
@@ -31,6 +31,7 @@ protected:
 	static CVector2D m_scroll;	//スクロール値
 	static bool m_wave_flag;	//今wave中ならtrue。敵全滅させたらfalseにして、スクロール開始
 	static 	bool m_sc_flag_x;	//プレイヤーが動いたことによるスクロールフラグ。trueならプレイヤーの動きは止まる
+	static 	int m_score;	//すこあ
 	int m_hp;  //体力
 	int m_at;  //攻撃力
 	int m_state; //状態
