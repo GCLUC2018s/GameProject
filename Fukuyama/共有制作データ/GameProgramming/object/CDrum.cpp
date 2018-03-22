b276fa9c9006bdd1e63786bfc80f9d33be46ea2e
Nagao-Ren-Fukuyama <17010511@anabuki-net.ne.jp>
2018-03-22 21:26:41 +0900

    ギミックの変更

--- Fukuyama/共有制作データ/GameProgramming/object/CDrum.cpp
+++ Fukuyama/共有制作データ/GameProgramming/object/CDrum.cpp
@@ -18,7 +18,7 @@ C_Drum::C_Drum()
 :C_Object(E_OBJ, eUDP_Object, eDWP_Object, DISPLAY_RIGHT, 0, 0)
 , m_DrumPos_Random(rand() % 3)
 {
-	mTexture.Load("object/Drum.tga");
+	mTexture.Load("image/object/Drum.tga");
 	//0の場合、真ん中に配置
 	if (m_DrumPos_Random == 0){
 		m_Position.z += Zpos_SetPos_Center;
@@ -31,39 +31,20 @@ C_Drum::C_Drum()
 	else if (m_DrumPos_Random == 2){
 		m_Position.z += Zpos_SetPos_Down;
 	}
+	m_image.SetVertex(-64, 64, -64, 64);
 }
 
 //更新処理
 void C_Drum::Update(){
-	m_image.SetVertex(-64, 64, -64, 64);
 	//スクロールする
 	Scroll(&m_Position, m_Scroll);
 	//画面左端に行った時
-	if (m_Position.x < DISPLAY_LEFT){
+	if (m_Position.x <=-670){
 		SetKill();
 		new C_Drum();
 	}
 	m_DrumAnimationCount--;
 	m_Position.x -= Drum_Move;
-	//アニメーション処理
-	if (m_DrumAnimationCount >= 24){
-		mTexture.DrawImage(m_image.m_Left, m_image.m_Right, m_image.m_Bottom, m_image.m_Top, 0, 128, 128, 0);
-	}
-	else if (m_DrumAnimationCount <= 24 && m_DrumAnimationCount >= 18){
-		mTexture.DrawImage(m_image.m_Left, m_image.m_Right, m_image.m_Bottom, m_image.m_Top, 128, 256, 128, 0);
-	}
-	else if (m_DrumAnimationCount <= 18 && m_DrumAnimationCount >= 12){
-		mTexture.DrawImage(m_image.m_Left, m_image.m_Right, m_image.m_Bottom, m_image.m_Top, 256, 384, 128, 0);
-	}
-	else if (m_DrumAnimationCount <= 12 && m_DrumAnimationCount >= 6){
-		mTexture.DrawImage(m_image.m_Left, m_image.m_Right, m_image.m_Bottom, m_image.m_Top, 384, 512, 128, 0);
-	}
-	else if (m_DrumAnimationCount <= 6 && m_DrumAnimationCount >= 0){
-		mTexture.DrawImage(m_image.m_Left, m_image.m_Right, m_image.m_Bottom, m_image.m_Top, 512, 640, 128, 0);
-	}
-	else{
-		m_DrumAnimationCount = 30;
-	}
 	//衝突判定
 	m_Colimage = m_image;
 
@@ -73,7 +54,7 @@ void C_Drum::Update(){
 bool C_Drum::Collision(CTask* p, CTask* q){
 	C_Object* ip_My = (C_Object*)p;
 	C_Object* ip_You = (C_Object*)q;
-	ip_My->m_Collision_Wide = 50;
+	ip_My->m_Collision_Wide = 60;
 	if (ip_You->m_id == E_PLAYER&&
 		C_Collider::Collision(ip_My, ip_You, &ip_My->m_Position, &ip_You->m_Position)){
 		ip_You->m_Position.x += ip_You->m_Colimage.m_AdjustX;
@@ -94,6 +75,25 @@ void C_Drum::Drum_Rect(C_Rectangle *image, const C_Vector3* pos){
 }
 //描画処理
 void C_Drum::Draw(){
+	//アニメーション処理
+	if (m_DrumAnimationCount >= 24){
+		mTexture.DrawImage(m_image.m_Left, m_image.m_Right, m_image.m_Bottom, m_image.m_Top, 0, 128, 128, 0);
+	}
+	else if (m_DrumAnimationCount <= 24 && m_DrumAnimationCount >= 18){
+		mTexture.DrawImage(m_image.m_Left, m_image.m_Right, m_image.m_Bottom, m_image.m_Top, 128, 256, 128, 0);
+	}
+	else if (m_DrumAnimationCount <= 18 && m_DrumAnimationCount >= 12){
+		mTexture.DrawImage(m_image.m_Left, m_image.m_Right, m_image.m_Bottom, m_image.m_Top, 256, 384, 128, 0);
+	}
+	else if (m_DrumAnimationCount <= 12 && m_DrumAnimationCount >= 6){
+		mTexture.DrawImage(m_image.m_Left, m_image.m_Right, m_image.m_Bottom, m_image.m_Top, 384, 512, 128, 0);
+	}
+	else if (m_DrumAnimationCount <= 6 && m_DrumAnimationCount >= 0){
+		mTexture.DrawImage(m_image.m_Left, m_image.m_Right, m_image.m_Bottom, m_image.m_Top, 512, 640, 128, 0);
+	}
+	else{
+		m_DrumAnimationCount = 30;
+	}
 	Drum_Rect(&m_image, &m_Position);
 }
 
