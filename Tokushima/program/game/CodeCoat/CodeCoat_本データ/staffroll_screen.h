586c2f615fe891a814ba73a8bd74590e90f3b64e
miyata-takuya-Tokushima <anabukimiyata3@gmail.com>
2018-03-22 17:02:21 +0900

    se

--- Tokushima/program/game/CodeCoat/CodeCoat_本データ/staffroll_screen.h
+++ Tokushima/program/game/CodeCoat/CodeCoat_本データ/staffroll_screen.h
@@ -6,9 +6,7 @@
 #include "Scene\SceneBase.h"
 
 const int GAMECLEAR_TIME = (46 * 1000);
-const int STRING_COUNT = 9;//流れる文字列の個数
-
-enum FONT_HANDLE{ A = 0,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z };
+const int STRING_COUNT = 12;//流れる文字列の個数
 
 //セレクトスクリーンクラス
 class CStaffRollScreen : public CSceneBase
@@ -28,7 +26,6 @@ private:
 	int m_largefont;
 	int m_lasttime;
 	int scroll[STRING_COUNT];
-	int m_font[26];
 	int m_rogoimage;
 	int clear_BGM;
 };
